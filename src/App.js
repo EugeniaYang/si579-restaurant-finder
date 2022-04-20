6c0f51fc3fcb1621589814fac380eb947ad8bf07
YenLen <b01612022@gmail.com>
2022-04-20 17:19:29 -0400

    add display marker function, enable unclick markers, customize list of restaurant

--- src/App.js
+++ src/App.js
@@ -1,87 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-import { Row, Col } from 'antd';
-import ResultListing from "./components/ResultListing2";
+import "./App.css";
+import { Row, Col } from "antd";
+import ResultListing from "./components/ResultListing";
 import React, { useEffect, useState } from "react";
 import SimpleMap from "./components/Map";
 import Complete from "./components/Complete";
-import Table from 'react-bootstrap/Table'
 
-
-
-const API_KEY = 'AIzaSyCPuTBdJFz2V9k_7QtyU6niWQg-irn84jk';
+const API_KEY = "AIzaSyCPuTBdJFz2V9k_7QtyU6niWQg-irn84jk";
 const findPos = (id) => {
-    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-    return fetch(proxyUrl + 'https://maps.googleapis.com/maps/api/place/details/json?' +
-        'place_id=' + id + '&fields=geometry' +
-        '&key=' + API_KEY, { mode: 'cors', headers: { 'Access-Control-Allow-Origin': "*" } })
-        .then(response => response.json())
-        .then((data) => {
-            return data.result.geometry.location;
-        }).catch((err) => {
-            console.log(err);
-        });
-
-}
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  return fetch(
+    proxyUrl +
+      "https://maps.googleapis.com/maps/api/place/details/json?" +
+      "place_id=" +
+      id +
+      "&fields=geometry" +
+      "&key=" +
+      API_KEY,
+    { mode: "cors", headers: { "Access-Control-Allow-Origin": "*" } }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return data.result.geometry.location;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 function App() {
-    const [centerPos, setCenterPos] = useState({
-        value: 'North Quadrangle Residential and Academic Complex, South State Street, Ann Arbor, MI, USA',
-        // place_id
-        key: "ChIJy_fxa0CuPIgRWmk6N0CQ0u8" 
-    });
-    const [centerLoc, setCenterLoc] = useState();
-    const [savedList, setSavedList] = useState([]);
+  const [centerPos, setCenterPos] = useState({
+    value:
+      "North Quadrangle Residential and Academic Complex, South State Street, Ann Arbor, MI, USA",
+    // place_id
+    key: "ChIJy_fxa0CuPIgRWmk6N0CQ0u8",
+  });
+  const [centerLoc, setCenterLoc] = useState();
+  const [marker, setMarker] = useState([]);
 
-    useEffect(async () => {
-        let loc = await findPos(centerPos.key);
-        console.log(loc)
-        setCenterLoc(loc)
-    }, [centerPos])
+  useEffect(async () => {
+    let loc = await findPos(centerPos.key);
+    console.log(loc);
+    setCenterLoc(loc);
+  }, [centerPos]);
 
-    useEffect(async () => {
-        let loc = await findPos(centerPos.key);
-        console.log(loc)
-        setCenterLoc(loc)
-    }, [])
-    return (
-        <div className="App">
-            <Row>
-                <Col xs={2} sm={3} md={3} lg={4} xl={5} />
+  useEffect(async () => {
+    let loc = await findPos(centerPos.key);
+    console.log(loc);
+    setCenterLoc(loc);
+  }, []);
+  return (
+    <div className="App">
+      <Row>
+        <Col xs={2} sm={3} md={3} lg={4} xl={5} />
 
-                <Col xs={20} sm={18} md={18} lg={16} xl={14}>
-                    <h1>Find the restaurant for you!</h1>
-                    <Complete setCenterPos={setCenterPos} />
-                    <div style={{
-                        width: '100%',
-                    }}>
-                        <h3>Current Center:{centerPos.value}</h3>
-                        <SimpleMap centerLoc={centerLoc} />
-                        
-
-                    </div>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <ResultListing  centerLoc={centerLoc} />
-                    <br/>
-                    <h3>
-                        Reference: <a href={'https://react-google-maps-api-docs.netlify.app/'}>React Google Maps Api Style Guide</a>
-                    </h3>
-
-                </Col>
-
-
-
-            </Row>
-
-
-        </div>
-    );
+        <Col xs={20} sm={18} md={18} lg={16} xl={14}>
+          <h1>Find the restaurant for you!</h1>
+          <Complete setCenterPos={setCenterPos} />
+          <div
+            style={{
+              width: "100%",
+            }}
+          >
+            <h3>Current Center:{centerPos.value}</h3>
+            <SimpleMap
+              centerLoc={centerLoc}
+              location={marker}
+              setLocation={setMarker}
+            />
+          </div>
+          <ResultListing
+            centerLoc={centerLoc}
+            setMarker={setMarker}
+            location={marker}
+          />
+          <br />
+          <h4>
+            Reference:{" "}
+            <a href={"https://react-google-maps-api-docs.netlify.app/"}>
+              React Google Maps Api Style Guide
+            </a>
+          </h4>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -2,53 +2,44 @@ import React, {useEffect, useState} from 'react';
 import {GoogleMap, Marker} from '@react-google-maps/api';
 
 
+// Customize marker
+const icon = { 
+    url: 'http://maps.google.com/mapfiles/kml/paddle/ylw-stars.png',
+};
 
 const SimpleMap = (props) => {
+    console.warn(props.location)
     const containerStyle = {
         width: '100%',
-        height: '400px'
+        height: '60%'
     };
-    const [markers,setMarkers]=useState([])
-    // const [centerPos, setCenterPos] = useState()
-    const [finish, setFinish] = useState(false)
 
+    // The current center marker 
     const ShowMarkers=()=>{
-        console.log('show')
-        // const centerLoc= centerPos;
-        console.log(props.centerLoc)
-
-        let result=<Marker position={props.centerLoc}/>;
-        
-
-
-        if (markers.len>0){
-
-        }
+        let result = <Marker position={props.centerLoc}/> 
         return result;
     }
 
-    // useEffect(async ()=>{
-    //     console.log('new center')
-    //     // let pos = await props.findPos(props.centerPos.key);
-    //     setFinish(true)
-    //     setCenterPos(pos)},[props])
+    // Customized markers user want to see 
+    const ShowMoreMarker=()=>{
+        return props.location.map ((location) =>{
+            return(<Marker icon = {icon} key={location.toString()} position={location} onClick={()=> {props.setLocation((current) => current.filter(element => element !==location))}}/>)  
+        })
+    }
 
-    // while (!finish) return (<div>Loading Map</div>)
+
     return (
-            // Important! Always set the container height explicitly
             <div style={{height: '450px', width: '100%'}}>
                 <GoogleMap
                     mapContainerStyle={containerStyle}
                     center={props.centerLoc}
-                    zoom={10}
+                    zoom={14}
                 >
                     {<ShowMarkers/>}
+                    {<ShowMoreMarker/>} 
                 </GoogleMap>
-
             </div>
         );
-
-
 }
 
 export default SimpleMap;
